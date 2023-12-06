9b6c0496a673d667f9b8c61b6d1a836c5015aa20
sreasgop <sreasgop@gmail.com>
2023-12-06 16:21:40 +0530

    51_DecimalToOctal.c added

---
+++ Chapter 10/51_DecimalToOctal.c
@@ -0,0 +1,22 @@
+// Write a program to enter a decimal number. Calculate and display the octal equivalent of this number. 
+
+#include <stdio.h>
+#include<math.h>
+
+int main() {
+    int user_input, remainder, octal = 0, i = 0; 
+    
+    printf("Enter a decimal number: ");
+    scanf("%d", &user_input);
+
+    while (user_input!=0) {
+        remainder = user_input % 8; 
+        octal += remainder * pow(10, i);
+        user_input = user_input / 8; 
+        i++;
+    }
+    
+    printf("%d", octal);
+
+    return 0;
+}
