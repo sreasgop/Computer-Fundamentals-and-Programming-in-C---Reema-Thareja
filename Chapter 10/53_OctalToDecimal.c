8d839cf27fb351e9fe5750a2f8aad715f94106ad
sreasgop <sreasgop@gmail.com>
2023-12-06 17:16:19 +0530

    53_OctalToDecimal.c modified: Formatting added

--- Chapter 10/53_OctalToDecimal.c
+++ Chapter 10/53_OctalToDecimal.c
@@ -2,15 +2,19 @@
 
 #include <stdio.h>
 int main() {
+
     int user_input, unit_digit, decimal, i=1; 
+    
     printf("Enter any octal number: ");
     scanf("%d", &user_input);
+    
     while(user_input!=0){
         unit_digit = user_input % 10; 
         decimal += unit_digit * i; 
         user_input/=10;
         i*=8;
     }
+    
     printf("Decimal: %d", decimal);
     return 0;
 }
